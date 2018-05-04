a9c9a3acf046c597d4246b1030376a63fb5a2c04
sneezie510 <shuynh1300@yahoo.com>
2018-05-04 16:37:04 +0200

    Refactored code. Switched to a messages service with MongoDB

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-var feathers = require('feathers');
-var mongodb = require('feathers-mongodb');
-var rest = require('feathers-rest');
-var socketio = require('feathers-socketio');
-var bodyParser = require('body-parser');
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
 
-var app = feathers();
-// On MongoDB use the `feathers-demo` database
-// and the `todos` collection
-var todoService = mongodb({
-  db: 'feathers-demo',
-  collection: 'todos'
-});
+const MongoClient = require('mongodb').MongoClient;
+const service = require('feathers-mongodb');
 
-// Configure REST and SocketIO endpointss
-app.configure(rest());
+// Create an Express compatible Feathers application instance.
+const app = express(feathers());
+// Turn on JSON parser for REST services
+app.use(express.json());
+// Turn on URL-encoded parser for REST services
+app.use(express.urlencoded({ extended: true }));
+// Enable REST services
+app.configure(express.rest());
+// Enable Socket.io
 app.configure(socketio());
-// Parse JSON and form HTTP bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// Use the Todo service object
-app.use('/todos', todoService);
 
-// Start the application on port 3030
-app.listen(3030, () => console.log('Feathers mongodb started at http://localhost:3030'));
+// Connect to the db, create and register a Feathers service.
+app.use('/messages', service({
+  paginate: {
+    default: 15,
+    max: 25
+  }
+}));
+
+// A basic error handler, just like Express
+app.use(express.errorHandler());
+
+// Connect to your MongoDB instance(s)
+MongoClient.connect('mongodb://localhost:27017/feathers')
+  .then(function (client) {
+    // Set the model now that we are connected
+    app.service('messages').Model = client.db('feathers').collection('messages');
+
+    // Now that we are connected, create a dummy Message
+    app.service('messages').create({
+      text: 'Message created on server'
+    }).then(message => console.log('Created message', message));
+  }).catch(error => console.error(error));
+
+// Start the server.
+const port = 3030;
+
+app.listen(port, () => {
+  console.log(`Feathers server listening on port ${port}`);
+});
